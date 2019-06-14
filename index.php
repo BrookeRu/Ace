2d11a755a72e1cc63ba5a804e8f2b6f76a24cd39
BrookeRu <30381387+BrookeRu@users.noreply.github.com>
2019-06-13 22:33:43 -0700

    left out
    final index

--- index.php
+++ index.php
@@ -1,37 +1,200 @@
-<?php
-try
-{
-  $pdo = new PDO('mysql:host=localhost;dbname=brookero_pht2', 'brookero_pht_user', 'myP4ssword');
-  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $pdo->exec('SET NAMES "utf8"');
-}
-catch (PDOException $e)
-{
-  $error = 'Unable to connect to the database server. Is this the right page?';
-  include  'includes/error.html.php';
-  exit();
-}
+<!DOCTYPE html>
+<!--
+File Name: index.html
+Date: 05/24/19
+Programmer: Brooke Roberts
+-->
 
-try
-{
-  $sql = 'SELECT * FROM content WHERE id=1';
-  $result = $pdo->query($sql);
-}
-catch (PDOException $e)
-{
-  $error = 'Error fetching pht: ' . $e->getMessage();
-  include 'includes/error.html.php';
-  exit();
-}
 
-while ($row = $result->fetch())
-{
-    $contents[] = array(
-        'title'=> $row['title'],
-        'description' => $row ['description'],
-        'image' => $row['image']
-    );
-}
+<html lang="en">
 
-include 'home/home.html.php';
-?>
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Ace in the Hole</title>
+
+    <!-- Poppins font -->
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet" type="text/css">
+    <link href="css/reset.css" rel="stylesheet" type="text/css">
+    <link href="css/helper.css" rel="stylesheet" type="text/css">
+    <link href="css/grid.css" rel="stylesheet" type="text/css">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <link href="css/reg.css" rel="stylesheet" type="text/css">
+    <!-- jQuery library DO NOT DELETE -->
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+    <!-- script folder needs to be linked DO NOT DELETE -->
+
+    <script src="scripts/custom.js"></script>
+</head>
+
+<body>
+
+    <?php include 'includes/header.inc.html.php';?>
+
+    <?php include 'includes/nav.inc.html.php';?>
+
+    <div id="wrapper">
+        <div class="hero_wrap">
+        </div>
+
+
+
+        <main>
+             <section>
+        
+                 <p class="index">
+                    The Annual Ace in the Hole Multisport Weekend is a legendary event in the Oregon triathlon and running community. It has become a traditional destination race for athletes from across the nation. <br><br>
+                    There is something for every level of athletic ability. The weekend includes a first timer triathlon, a sprint, Olympic, and Half-Iron triathlons and 10K and Half marathon runs. Come to experience your first race or come to compete to win, but make sure you come to have fun!</p><br>
+            
+               
+                <h2>COURSE DETAILS</h2><br><br>
+                
+                
+                <h4>Long Course SWIM</h4><br>
+                
+                <p>WATER TEMPERATURE is expected to be between 62 - 66 degrees. The temperature will be taken on Friday and the morning of the race. Wetsuits are recommended.</p><br>
+                
+                <p>1.2mi - Participants will make two counter-clockwise loops. Large buoys will mark the turn points. Kayakers will be positioned on the water to support the swimmers. Medical support will be present on the beach.</p><br><br>
+
+                <h4>Long Course BIKE</h4> <br>
+                <p>58 Miles: A scenic point-to-point course that travels over gently rolling hills prior to three hard climbs. The bike course will be marked with large directional signage and there will be course marshals at key intersections to help direct you.</p> <br><br>
+                
+                <h4>Long Course RUN</h4><br>
+                <p>13.1mi - A mostly flat loop course on widely paved bike paths that traverse through and around this beautiful and scenic destination resort (two hills total with a minimal elevation gain).</p><br><br>
+                
+                <h4>OLYMPIC SWIM</h4><br>
+                <p>1,500 meters - Participants will make two counter-clockwise loops. Large buoys will mark the turn points. Kayakers will be positioned on the water to support the swimmers. Medical support will be present on the beach.</p><br><br>
+                
+                <h4>OLYMPIC BIKE</h4><br>
+                <p>28mi - A scenic point-to-point course that travels over gently rolling hills. The bike course will be marked with large directional signage and course marshals will be present at key intersections.</p> <br><br>
+                
+                <h4>OLYMPIC RUN</h4>
+                
+                <p>10K - A mostly flat loop course on widely paved bike paths that traverse through and around this beautiful and scenic destination resort (one hill total with a minimal elevation gain).</p><br><br>
+                
+                <h4>Sprint</h4>
+                
+                <p>Course will offer a 1-loop 1/2 mile swim, the exact same 28 mile Bike Course as the Olympic distance and a 5km run.</p><br><br>
+
+                <h4>Try-A-Tri</h4> <br>
+                
+                <p>This novice race is designed for the first time triathlete, those new to the sport, our Junior Triathletes. The swim is a shorter, more manageable 1/4 mile distance, (instead of the standard 1/2-mile Sprint distance swim), 10 mile bike ride (vs 12 miles and it's a 2 loop course making it very spectator friendly!) and a flat 2 mile run (vs 3 mile sprint course).</p><br><br>
+                
+                <h4>Half Marathon COURSE</h4><br>
+                
+                <p>Half-Marathon (13.1-miles): Half-Marathon event starts and finishes in the Athletes Village to the cheers of the enthusiastic crowd. Once finished, runners can enjoy the finish line festivities, including the Sports & Fitness Expo and live entertainment. Post-race refreshments will be provided and the Awards Ceremony for the Half-Marathon will begin once the results have been certified.</p><br><br>
+                
+                <h4>10k COURSE</h4><br>
+                
+                <p>The 10K event starts and finishes in the Athletes Village. The paths are approximately 6 ft wide, perfectly paved and wind around through the forest. Each course has only two small hills with a minimal elevation gain to navigate and a fast downhill to flat finish to the roaring cheers of the crowd.</p><br><br>
+                
+                <h4>Splash n' Dash</h4> 
+                
+                <p>Kids event for participants ages 4 to 15. This is a 100 yard swim and a 50 yard dash.</p><br><br>
+
+
+                <p><a href="https://www.youtube.com/watch?v=-32OlsSH6q8" target="_blank">Click here for video on How To Train For Your First Triathlon!
+                    </a>
+                </p>
+
+            </section>
+            <section id="slide">
+
+            <h3>Photos of Events & Athletes</h3>
+            <img class="mobile-slide" src="images/bike_sunrise.jpg">
+            <div class="slideshow-container tablet-slide">
+
+                <div class="mySlides fade">
+                    <img src="images/competition-609874.jpg" title="Triathlon Swim Event" alt="Ace in the Hole Multisport Events
+                     - triathlon swimming event Portland OR" style="width:100%">
+                    <div class="text">Triathlon Swim Event</div>
+                </div>
+
+                <div class="mySlides fade">
+                    <img src="images/approve.jpg" title="Wheelchair racer" alt="Ace in the Hole Multisport Events
+                     - triathlon biking event, athlete using wheelchair, all participants welcome, Portland OR" style="width:100%">
+                    <div class="text">Triathlon Bike Event</div>
+                </div>
+
+                <div class="mySlides fade">
+                    <img src="images/cycling.jpg" title="Cycling" alt="Ace in the Hole Multisport Events
+                     - triathlon cycling event Portland OR" style="width:100%">
+                    <div class="text">Triathlon Run Event</div>
+                </div>
+
+                <div class="mySlides fade">
+                    <img src="images/triathlon-452572.jpg" title="Triathlon runners" alt="Ace in the Hole Multisport Events
+                     - Annual Event Weekend in Portland OR" style="width:100%">
+                    <div class="text">Triathlon Participants</div>
+                </div>
+
+                <div class="mySlides fade">
+                    <img src="images/022312_Hagg_Lake_swart_600.jpeg" title="Triathlon Swim Event location at Hagg Lake" alt="Ace in the Hole Multisport Events
+                     - start of triathlon swimming event Portland OR" style="width:100%">
+                    <div class="text">Hagg Lake</div>
+                </div>
+
+                <div class="mySlides fade">
+                    <img src="images/competition-609874.jpg" title="Hagg lake swimmers" alt="Ace in the Hole Multisport Events
+                     - swim race, Portland OR" style="width:100%">
+                    <div class="text">Triathlon Swim Event</div>
+                </div>
+
+                <div class="mySlides fade">
+                    <img src="images/runner-662826.jpg" title="Triathlon Run Event" alt="Ace in the Hole Multisport Events
+                     - triathlon running event, athlete in racing wheelchair, all participants welcome, Portland OR" style="width:100%">
+                    <div class="text">Triathlon Run Event</div>
+                </div>
+
+                <div class="mySlides fade">
+                    <img src="images/bike-cycling-trail-cyclist-160949.jpg" title="Cyclist on trail"
+                         alt="Ace in the Hole Multisport Events - all participants welcome, Portland OR" style="width:100%">
+                   
+                    <div class="text">Cycling Event</div>
+                </div>
+
+                <div class="mySlides fade">
+                    <img src="images/pexels-photo.jpeg" title="Lone Swimmer" alt="Ace in the Hole Multisport Events
+                     - triathlon swimming event, all inclusive, Portland OR" style="width:100%">
+                    <div class="text">Triathlon Bike Event</div>
+                </div>
+
+                <div class="mySlides fade">
+                    <img src="images/triathalon-cycling-racer-618750.jpg" title="Triathlon Bike Event" alt="Ace in the Hole Multisport Events
+                     - triathlon biking event all inclusive Portland OR" style="width:100%">
+                    <div class="text">Triathlon Run Event</div>
+                </div>
+
+            </div>
+            <br>
+            <div class="tablet">
+                <div id="dotLine">
+                    <span class="dot"></span>
+                    <span class="dot"></span>
+                    <span class="dot"></span>
+                    <span class="dot"></span>
+                    <span class="dot"></span>
+                    <span class="dot"></span>
+                    <span class="dot"></span>
+                    <span class="dot"></span>
+                    <span class="dot"></span>
+                    <span class="dot"></span>
+                    <span class="dot"></span>
+                </div>
+            </div>
+                <br></section>
+
+        </main>
+
+        <?php include 'includes/footer.inc.html.php';?>
+    </div>
+
+</body>
+
+</html>
